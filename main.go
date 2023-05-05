7e2a3fa7f1aeca716c2dec3667c230ae9577025b
Vernon Grant <vernon@ruppell.io>
2023-05-05 21:56:53 +0200

    Adds problem number

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 )
 
-// Maximum number of vowels in a substring of given length.  Given a string s
-// and an integer k, return the maximum number of vowel letters in any substring
-// of s with length k.  Vowel letters in English are 'a', 'e', 'i', 'o', and
-// 'u'.
+// 1456: Maximum number of vowels in a substring of given length.  Given a
+// string s and an integer k, return the maximum number of vowel letters in any
+// substring of s with length k.  Vowel letters in English are 'a', 'e', 'i',
+// 'o', and 'u'.
 //
 // TODO: There's a lot of performance to gain in string rune access. Run some
 // benchmarks for string rune access operations.
